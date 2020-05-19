97472435f03f2b79d07f72ae07bb26b1b3bba488
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-19 02:22:36 -0700

    Removed dense layers
    major overfitting caused by dense layers, perhaps more and more diverse data will solve this issue

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import random
 import time
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, BatchNormalization
 
 
@@ -92,50 +92,37 @@ print(len(test_X))
 print("val labels length:")
 print(len(test_y))
 
-train_X = np.array(train_X).reshape(reshape)
-test_X = np.array(test_X).reshape(reshape)
+train_X = np.clip(np.array(train_X).reshape(reshape) - np.mean(train_X), -10, 10) / 10
+test_X = np.clip(np.array(test_X).reshape(reshape) - np.mean(test_X), -10, 10) / 10
 
 train_y = np.array(train_y)
 test_y = np.array(test_y)
 
-model = Sequential() 
+model = Sequential()
 
-model.add(Conv1D(64, (3), input_shape=train_X.shape[1:]))
+model.add(Conv1D(64, (5), padding='same', input_shape=train_X.shape[1:]))
 model.add(Activation('relu'))
 
-
-model.add(Conv1D(128, (2)))
+model.add(Conv1D(128, (5), padding='same'))
 model.add(Activation('relu'))
 
-model.add(Conv1D(128, (2)))
+model.add(Conv1D(256, (5), padding='same'))
 model.add(Activation('relu'))
 
-model.add(Conv1D(64, (2)))
+model.add(Conv1D(512, (5), padding='same'))
 model.add(Activation('relu'))
-model.add(MaxPooling1D(pool_size=(2)))
-model.add(Dropout(0.2))
 
-model.add(Conv1D(64, (2)))
-model.add(Activation('relu'))
-model.add(MaxPooling1D(pool_size=(2)))
-model.add(Dropout(0.2))
-
-model.add(Flatten())
-
-model.add(Dense(512))
-model.add(Dense(256))
-model.add(Dense(128))
-
-model.add(Dense(3))
+model.add(Conv1D(3, (16)))
+model.add(Reshape((3,)))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# epochs = 10
-# batch_size = 32
-# #print("executing training iteration " + str(itteration))
-# model.fit(train_X, train_y, batch_size=batch_size, epochs=epochs, validation_data=(test_X, test_y))
-# #model_name = f"new_models/{round(score[1]*100,2)}-acc-64x3-batch-norm-{epoch}epoch-{int(time.time())}-loss-{round(score[0],2)}.model"
-# #model.save(model_name)
-# #print("saved:")
-# #print(MODEL_NAME)
+epochs = 10
+batch_size = 64
+#print("executing training iteration " + str(itteration))
+model.fit(train_X, train_y, batch_size=batch_size, epochs=epochs, validation_data=(test_X, test_y))
+#model_name = f"new_models/{round(score[1]*100,2)}-acc-64x3-batch-norm-{epoch}epoch-{int(time.time())}-loss-{round(score[0],2)}.model"
+#model.save(model_name)
+#print("saved:")
+#print(MODEL_NAME)
