566375e02c9b96579ae009c68a05a29478dc03b7
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-22 19:24:28 -0700

    Pull data from EEG sensor array using brainflow

--- data_collection_frame.py
+++ data_collection_frame.py
@@ -2,3 +2,5 @@
 ### paramaters reading from both the OpenBCI headset and the Spektrum
 ### controller
 ### Author: Saurav Shroff
+### READ: make sure to locally run "python -m pip install brainflow" otherwise (@NicholasWeaver) "you're GONNA have a BAD DAY"
+
