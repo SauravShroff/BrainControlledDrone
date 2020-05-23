ee04a6fda64fcd4cc3d0ef9ee3e8abe1f7608fe3
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-23 15:49:06 -0700

    Correct brainflow params

--- data_collection_frame.py
+++ data_collection_frame.py
@@ -3,4 +3,13 @@
 ### controller
 ### Author: Saurav Shroff
 ### READ: make sure to locally run "python -m pip install brainflow" otherwise (@NicholasWeaver) "you're GONNA have a BAD DAY"
+### Visit: https://brainflow.readthedocs.io/en/stable/UserAPI.html for documentation on the Cyton/Daisy board SDK
 
+import time
+import numpy as np
+
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+board_id = BoardIds.CYTON_DAISY_BOARD
