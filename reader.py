4f83a0e67403e732637a9041693d45e6e58dfd48
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-25 14:41:58 -0700

    Run with sudo for access

--- reader.py
+++ reader.py
@@ -1,7 +1,10 @@
 ### This file is created to read data from the drone controller via USB.
 ### Due to the fact that only a single application can read rom an application,
-### this file will then pass along its inputs to the autoquad1 drone sim made by UAVS@Berkeley (thanks guys!)
-### make sure to pip install pyusb :)
+### this file will then pass along its inputs to the autoquad1 drone sim made by UAVS@Berkeley. (thanks guys!)
+### Make sure to pip install pyusb :)
+### Run with sudo (unless u want to be denied access)
 ### Author: Saurav Shroff
 
 import usb.core
+
+dev = 
