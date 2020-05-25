677914d065f2d01f9eda2ff758a610afe275228a
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-25 14:51:06 -0700

    Updated reader to work generically with any USB device
    requires ID_VENDOR and ID_PRODUCT

--- reader.py
+++ reader.py
@@ -2,9 +2,27 @@
 ### Due to the fact that only a single application can read rom an application,
 ### this file will then pass along its inputs to the autoquad1 drone sim made by UAVS@Berkeley. (thanks guys!)
 ### Make sure to pip install pyusb :)
-### Run with sudo (unless u want to be denied access)
+### Run with sudo https://www.urbandictionary.com/define.php?term=sudo
 ### Author: Saurav Shroff
 
 import usb.core
 
-dev = 
+ID_VENDOR = 0x0000 # Find id_vendor and id_prodcut by using lsusb on the command line
+ID_PRODUCT = 0xFFFF
+NUM_BYTES = 1024
+
+dev = usb.core.find(idVendor = ID_VENDOR, idProduct = ID_PRODUCT)
+endpoint = dev[0].interfaces()[0].endpoints()[0]
+i = dev[0].interfaces()[0].bInterfaceNumber
+dev.reset()
+
+if dev.is_kernel_driver_active(i):
+    dev.detach_kernel_driver(i)
+
+dev.set_configuration()
+endpointadr = endpoint.bEndpointAddress
+
+ret = dev.read(endpointadr, NUM_BYTES)
+
+print(len(ret))
+print(ret)
