8e6caa637bab9ad2b62b2ed7ed199ea7fc2927fb
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-26 20:42:14 -0700

    Use pygame to read inputs from xbox controller

---
+++ pygame_reader.py
@@ -0,0 +1,3 @@
+### 
+### Run with sudo https://www.urbandictionary.com/define.php?term=sudo
+### Author: Saurav Shroff
--- reader.py
+++ reader.py
@@ -7,17 +7,25 @@
 
 import usb.core
 
-ID_VENDOR = 0x25a7 # Find id_vendor and id_prodcut by using lsusb on the command line
-ID_PRODUCT = 0xfa23
+ID_VENDOR = 0x045e # Find id_vendor and id_prodcut by using lsusb on the command line
+ID_PRODUCT = 0x02d1
 NUM_BYTES = 1024
 
 dev = usb.core.find(idVendor = ID_VENDOR, idProduct = ID_PRODUCT)
+print("dev:")
+print(dev)
 endpoint = dev[0].interfaces()[0].endpoints()[0]
+print("endpoint:")
+print(endpoint)
 i = dev[0].interfaces()[0].bInterfaceNumber
+print("i:")
+print(i)
 dev.reset()
+print("successfully reset")
 
 if dev.is_kernel_driver_active(i):
     dev.detach_kernel_driver(i)
+print("detach confirmed")
 
 dev.set_configuration()
 endpoint_adr = endpoint.bEndpointAddress
