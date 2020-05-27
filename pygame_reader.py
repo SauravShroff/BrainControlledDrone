ae1bd5efba825a48e005a567b8d282ca5f2812fd
Saurav Shroff <sauravshroff@bekreley.edu>
2020-05-26 20:49:09 -0700

    documentation update :)
    In a REALLY good mood right now <3

--- pygame_reader.py
+++ pygame_reader.py
@@ -1,3 +1,4 @@
-### 
-### Run with sudo https://www.urbandictionary.com/define.php?term=sudo
+### This file uses pygame to read inputs from the xbox controller
+### which acts as our "drone controller" for the purposes of simlation/data collection
+### Run with sudo https://www.urbandictionary.com/define.php?term=sudo SUPER USER DO :):):)!!!! (im in a good mood right now)
 ### Author: Saurav Shroff
