55a1c04909e07cb8afd0cda1c61b90e950266ce4
Saurav <sauravshroff@berkeley.edu>
2020-06-02 01:00:35 -0700

    Useful scripts for testing (connect and disconnect API from sim)

--- dronecontrol.py
+++ dronecontrol.py
@@ -5,3 +5,14 @@ client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
+
+
+f1 = client.takeoffAsync()
+f1.join()
+
+state1 = client.getMultirotorState()
+s = print(state1)
+
+airsim.wait_key('Press any key to move vehicles')
+f1 = client.moveToPositionAsync(-5, 5, -10, 5)
+f1.join()
