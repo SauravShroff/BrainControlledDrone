63ab58b2c10a53e33e2f87bfabd18389b81e498f
Saurav <sauravshroff@berkeley.edu>
2020-06-02 14:19:17 -0700

    Good morning

--- dronecontrol.py
+++ dronecontrol.py
@@ -5,14 +5,3 @@ client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
-
-
-f1 = client.takeoffAsync()
-f1.join()
-
-state1 = client.getMultirotorState()
-s = print(state1)
-
-airsim.wait_key('Press any key to move vehicles')
-f1 = client.moveToPositionAsync(-5, 5, -10, 5)
-f1.join()
