12481f783b72485c427afbf8e8eb6a6756752764
Saurav <sauravshroff@berkeley.edu>
2020-06-08 19:08:59 -0700

    Added additional analytics features

--- brain_data_analytics.py
+++ brain_data_analytics.py
@@ -1,9 +1,9 @@
 # fill in an accurate file path
 import numpy as np
 
-file_path = "D:/model_data/1591659935 to 1591659941/1b.npy"
+file_path = "D:/model_data/1591659935 to 1591659941"
 
-brain_data_package = np.load(file_path)
+brain_data_package = np.load(file_path + "/1b.npy")
 print(brain_data_package.shape)
 num_frames = brain_data_package.shape[1]
 
@@ -14,3 +14,9 @@ for i in range(num_frames - 1):
 
 print("Of " + str(num_frames) + " frames, " +
       str(duplicates) + " are duplicates")
+
+
+analytics = np.load(
+    file_path + "/analytics.npy").tolist()
+print("the recording started at " + str(analytics[0]) + ", ended at " + str(
+    analytics[1]) + ", and lasted " + str(analytics[2]) + " seconds, with an average FPS of " + str(analytics[3]) + ".")
