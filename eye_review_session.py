8ba2b414c3080ab40b17c91a2acb088495e37082
Saurav <sauravshroff@berkeley.edu>
2020-06-22 15:51:08 -0700

    Allow user to generate new guesses based on specified model

--- eye_review_session.py
+++ eye_review_session.py
@@ -1,12 +1,27 @@
+import tensorflow as tf
 import numpy as np
 
-file_path = "D:/eye_model_data/1592702292 to 1592702359"
+# Define user params
+FILE_PATH = "D:/eye_model_data/1592702292 to 1592702359"
+REGENERATE_GUESS = True
+MODEL_NAME = "Sarah_Only"
+DELETE_CHANGES = False
 
-true_eye = np.load(file_path + "/2c.npy")
-guess_eye = np.load(file_path + "/3g.npy")
-print(true_eye.shape)
-print(guess_eye.shape)
+# labels
+true_eye = np.load(FILE_PATH + "/2c.npy")
 
+# either make new guesses based on brain data using a model speicified in MODEL_NAME
+# or pull guesses that were made at record-time
+if REGENERATE_GUESS:
+    model = tf.keras.models.load_model("D:/eye_models/" + MODEL_NAME)
+    guess_eye = model(np.load(FILE_PATH + "/1b.npy"))
+else:
+    guess_eye = np.load(FILE_PATH + "/3g.npy")
+
+# make sure the true values and precicted values are of the same shape
+assert(true_eye.shape == guess_eye.shape)
+
+# compute accuracy
 total_frames = true_eye.shape[0]
 correct_frames = 0
 for i in range(total_frames):
