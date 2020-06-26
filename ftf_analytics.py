58d2ed6bf951cc7c234634840be4f55e55d24ba3
Saurav <sauravshroff@berkeley.edu>
2020-06-26 10:17:55 -0700

    Minor change

--- ftf_analytics.py
+++ ftf_analytics.py
@@ -3,14 +3,13 @@ from numpy.linalg import norm
 
 
 relevant_channel = 4
-a = np.load("D:/drone_model_data/1591659935 to 1591659941/1b.npy")
-a = a[0]
+a = np.load("D:/drone_model_data/Saurav/1592253726 to 1592254011/1b.npy")
 print(a.shape)
 
-i = 16
+i = 50
 current_image = a[i]
 current_vector = current_image[relevant_channel]
-next_image = a[i+1]
+next_image = a[i+5]
 cos_sims = []
 for forward_vector in next_image:
     cos_sim = np.dot(forward_vector, current_vector) / \
