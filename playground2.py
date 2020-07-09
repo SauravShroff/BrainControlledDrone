ec65b7046079fbcc400bdd0cc36ef7ca84be0001
Saurav <sauravshroff@berkeley.edu>
2020-07-09 00:51:30 -0700

    goodnight

--- playground2.py
+++ playground2.py
@@ -1,17 +1,7 @@
 import numpy as np
 import helpers.process_array as process
 # import tensorflow as tf
-
-array = np.arange(720)
-array = np.reshape(array, (8, 9, 10))
-
-
-array2 = np.arange(720)
-array2 = np.reshape(array, (8, 9, 10))
-
-newarray = array[1:3]
-# print(newarray)
-
-
-for i in range(10):
-    print(i)
+from joblib import dump, load
+MODEL_NAME = "hello"
+models = load("D:/drone_models/" + MODEL_NAME)
+print(models)
